3d9fdb25a3015b53189cd25fea26c87c653c26b2
ohinibla <ohinibla@gmail.com>
2025-05-06 22:37:03 +0330

    fixed utils
    add a new check for getting ban time remaining
    changed function names

--- interview/users/utils.py
+++ interview/users/utils.py
@@ -12,13 +12,26 @@ def get_ban_remaining_time(identifier):
     Returns remaining ban time saved in cache based on identifier as cache key
     if no cache value found, return a default
     """
+    # if `identifier` is None (cache not set yet), don't even check
+    if identifier is None:
+        return 0
     user_ban_start_time = cache.get(identifier, DEFAULT_USER_STAT).get("ban_start_time")
+    # calculate user's remaining ban
+    # if user_ban_start_time is None, then the user is not banned, so return 0
     remaining_time_minutes = (
         BAN_PERIOD_MINUTE - (datetime.now() - user_ban_start_time).seconds / 60
         if user_ban_start_time
         else 0
     )
     return remaining_time_minutes
+
+
+def check_is_user_banned(identifier):
+    """
+    Returns True if user remaining ban time is other that 0, which
+    should mean than user is still banned
+    """
+    return bool(get_ban_remaining_time(identifier))
 
 
 def ban_user_if_necessary(identifier):
