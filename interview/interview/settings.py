af20ff2228e8141b00c6f95c88746435306fcfa5
ohinibla <ohinibla@gmail.com>
2025-05-06 19:27:08 +0330

    allowed host * for testing

--- interview/interview/settings.py
+++ interview/interview/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9kc0c_ug*3=55fcrn!s04pyn_@(6)!z2gn%_lf+!j4lfbg**pb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
