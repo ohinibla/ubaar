a74e10365a4ab6ebf485cc5fde8afc5525af054b
ohinibla <ohinibla@gmail.com>
2025-05-06 19:26:36 +0330

    rearranged otp.py

--- interview/users/otp.py
+++ interview/users/otp.py
@@ -1,11 +1,11 @@
-class OTP:
+def send_sms(phonenumber):
+    print(f"send message to {phonenumber}")
 
-    @staticmethod
-    def send_sms(phonenumber):
-        print(f"send message to {phonenumber}")
 
-    @staticmethod
-    def generate_random_otp():
-        from random import randint
+def generate_random_otp():
+    """
+    Generate a random 6-digit OTP as string
+    """
+    from random import randint
 
-        return "".join([str(randint(0, 9)) for _ in range(6)])
+    return "".join([str(randint(0, 9)) for _ in range(6)])
